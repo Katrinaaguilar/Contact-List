e60b0c4b212e6a1b583d1d4d25e01c250242e4ab
KatrinaDawnAguilar <kat15aguilar@yahoo.com>
2018-03-07 13:55:03 +0800

    adding a document in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 var firebaseConfig = {
   apiKey: "",
