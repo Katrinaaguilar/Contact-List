8cadcd63f615d5e298d4b7faeecc02235e330124
KatrinaDawnAguilar <kat15aguilar@yahoo.com>
2018-03-07 10:03:25 +0800

    add codes to retrieved a collection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,25 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Observable }  from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+interface User {
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userCol: AngularFirestoreCollection<User>;
+  user: Observable<User[]>;
 
  constructor(private afs: AngularFirestore){}
 
   ngOnInit() {
+    this.userCol = this.afs.collection('user');
+    this.user = this.userCol.valueChanges();
 
   }
  
